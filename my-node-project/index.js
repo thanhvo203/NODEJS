62e7c544889bc35d3bbce08acb5af01679fa3192
thanhvo203 <vohaithanh21@gmail.com>
2023-11-30 10:35:02 +0700

    fix

--- my-node-project/index.js
+++ my-node-project/index.js
@@ -1,6 +1,15 @@
 const http = require('http');
+const fs = require('fs');
+const e = require('cors');
 const server = http.createServer((req,res) => {
-    res.write('Hello world');
-    res.end();
+        fs.readFile('index.html',(err,data) => {
+            if(err) {
+                res.statusCode = 500;
+                res.end('Error');
+            }else{
+                res.setHeader('Content-Type','text/html');
+                res.end(data);
+            }
+        })
 }) 
 server.listen(4000);
